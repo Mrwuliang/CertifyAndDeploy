985fe663f15b17039341d4fc05119c03111449fe
admin <fooldove@outlook.com>
2025-08-06 13:29:17 +0800

    update
    Signed-off-by: admin <fooldove@outlook.com>

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     # 您可以在这里配置 host 和 port
     # 例如: app.run(host='0.0.0.0', port=80, debug=True)
     # app.run(debug=True)
-    socketio.run(app, debug=True, port=80)
+    socketio.run(app, debug=True)
--- web/web_controller.py
+++ web/web_controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 from web import socketio
-from .web_service import index, config, update, apply_ssl_logic, ssl_detail, deploy_certificate, shell
+from .web_service import index, config, update, apply_ssl_logic
 
 # 创建一个名为 'web' 的蓝图
 web_bp = Blueprint('web', __name__)
@@ -25,13 +25,3 @@ def _update():
 @socketio.on('/apply_ssl')
 def _ssl(ssl_data):
     return apply_ssl_logic(ssl_data)
-
-
-@web_bp.route('/getSslDetail/<ssl_id>')
-def _ssl_detail(ssl_id):
-    return ssl_detail(ssl_id)
-
-
-@web_bp.route('/download', methods=['POST'])
-def download():
-    return deploy_certificate()
